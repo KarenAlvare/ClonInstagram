ee8c6d7e90b0c0c1eba060b4985f4e4f2bdcff9d
KarenAlvere <alvarezpalafoxanakaren@gmail.com>
2023-12-06 20:51:44 -0600

    Tersera Parte

---
+++ app/src/main/java/com/example/instagram/data/Story.kt
@@ -0,0 +1,8 @@
+package com.example.instagram.data
+
+import android.provider.ContactsContract.CommonDataKinds.Nickname
+
+class Story (
+    val userNickname: String,
+    val userAvatar: String
+)
